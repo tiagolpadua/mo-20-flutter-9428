f368622e28e5b9f5169e3c5bc5688c313d838762
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-09-13 20:40:28 -0300

    2.7

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('200.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-            ],
-          ),
+          body: TransfersList(),
           appBar: AppBar(
             title: Text('Transfers'),
           ),
@@ -30,3 +13,38 @@ void main() => runApp(
         ),
       ),
     );
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      TransferItem(Transfer(100.0, 1000)),
+      TransferItem(Transfer(200.0, 2000)),
+      TransferItem(Transfer(300.0, 3000)),
+    ]);
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+}
