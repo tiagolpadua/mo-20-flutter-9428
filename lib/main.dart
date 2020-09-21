1066f55228d54169d9aca0e2d4a8580e37ad34a0
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-09-20 23:32:55 -0300

    8.7

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(BytebankApp());
+void main() {
+  runApp(BytebankApp());
+
+  save(Contact(0, 'alex', 1000)).then((id) {
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
+}
 
 class BytebankApp extends StatelessWidget {
   @override
